edfeb408ed0fa54da4c8c4d704bf70312ad6d052
unknown <sergeyandmelnikov@gmail.com>
2022-08-17 23:47:03 +0300

    added role verification

--- src/app/auth.js
+++ src/app/auth.js
@@ -2,18 +2,36 @@
 
 	const authBtn = document.querySelector('form.auth input[type="submit"]');
 
+	var getLoginRole = function(storage, value) {
+		let users = Object.values(storage);
+	
+		for (var i = 0; i < users.length; i++) {
+			if(Object.values(users[i]).includes(value)) {
+				return users[i].role;
+			};
+		}
+	}
+
 	authBtn.addEventListener('click', (e) => {
 
 		e.preventDefault();
 
 		const loginEmailField = document.querySelector('#email');
 		const passwordField = document.querySelector('#password');
+
+		let loginRole = getLoginRole(auth, loginEmailField.value);
+		let role = Object.keys(auth)[loginRole];
 		
-		if(loginEmailField.value == auth.user.email && passwordField.value == auth.user.password) {
-			document.location.href = 'tasks.html';
+		if(role) {
+			if(loginEmailField.value == auth[role].email && passwordField.value == auth[role].password) {
+				document.cookie = role + '=true;secure;samesite=strict;max-age=3600';
+				document.location.href = 'tasks.html';
+			} else {
+				document.querySelector('#message').innerHTML = 'Заполните поля верными данными';
+			}
 		} else {
-			document.querySelector('#message').innerHTML = 'Заполните поля верными данными';
-		} 
+			document.querySelector('#message').innerHTML = 'Для ' + (loginEmailField.value || 'вас') + ' доступ закрыт!';
+		}
 	});
 
 }(document));
--- src/settings/env.js
+++ src/settings/env.js
@@ -1,11 +1,19 @@
 let auth = {
 	"superuser" :  {
 		"email" : "superuser@box.tool",
-		"password" : "password"
+		"password" : "password",
+		"role" : 0
 	},
 
 	"user" :  {
 		"email" : "user@box.tool",
-		"password" : "password"
+		"password" : "password",
+		"role" : 1
+	},
+
+	"beholder" :  {
+		"email" : "beholder@box.tool",
+		"password" : "password",
+		"role" : 2
 	}
 }
